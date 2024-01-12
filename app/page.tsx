c83376dd844d001cd5d4c2ffeb728904f58f6f72
Bayram Çınar <bayram200212@gmail.com>
2024-01-12 12:18:03 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
 import CardSlider from "../components/cardSlider";
-export default function Page() {
-  return <CardSlider />;
+
+export const metadata = {
+  title: "Ofistik || Hizmet Al - Hizmet Ver",
+  description: "İhtiyacınıza yönelik tüm işleri Ofistik üzerinden temin edin.",
+};
+
+export default function page() {
+  return (
+    <>
+      <CardSlider />
+    </>
+  );
 }
